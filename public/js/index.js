8ef19c4bf12bd9fddd14f9c0b0d4f33a6835bb86
abhishek <abugautam@gmail.com>
2018-04-17 07:35:03 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,7 @@
 var socket=io();
 socket.on('connect',function(){
 	console.log('Connected to server');
-
-	socket.emit('createMessage',{
-		from : 'Gauti',
-		text : 'Hey, I am Gauti!!'
-	});
+	
 });
 
 socket.on('disconnect',function(){
