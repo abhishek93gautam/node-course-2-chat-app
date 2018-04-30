6e4c00973f2bbaa79f5409de05920e84c4dbbf76
abhishek <abugautam@gmail.com>
2018-04-30 21:32:52 +0530

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage,generateLocationMessage} = require("./utils/message");
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname,'../public');
 const port =process.env.PORT || 3000;
@@ -12,6 +14,7 @@ const port =process.env.PORT || 3000;
 var app= express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 
 app.use(express.static(publicPath));
@@ -19,9 +22,22 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 	console.log('New user connected');
 
-	socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app.'));
+	socket.on('join',(params,callback)=>{
+		if(!isRealString(params.name) || !isRealString(params.room))
+		{
+			return callback('Name and room name are required.')
+		}
 
-	socket.broadcast.emit('newMessage',generateMessage('Admin','New user connected'));
+		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id,params.name,params.room);
+
+		io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+		socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app.'));
+
+		socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
+		callback();
+	});
 
 	socket.on('createMessage',(message, callback)=>{
 		console.log('Create message',message);
@@ -39,7 +55,14 @@ io.on('connection',(socket)=>{
 	});
 
 	socket.on('disconnect',()=>{
-		console.log('Client disconnected');
+		var user = users.removeUser(socket.id);
+
+		if(user)
+		{
+			io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+			io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left.`));
+		}
+		
 	});
 });
 
